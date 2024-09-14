c9ca0b5a58caf0627cbf6f9cc15b989f77fa9f54
iamedisonu <iamedisonu250@gmail.com>
2024-09-14 16:36:41 -0500

    web scraper

--- aiweb.py
+++ aiweb.py
@@ -1,7 +1,10 @@
 import streamlit as st
+from scrape import scrape_website
 
 st.title("AI Web Scraper")
 url =st.text_input("Enter a website URL: ")
 
 if st.button("Scrape site"):
     st.write("scraping the website")
+    result = scrape_website(url)
+    print(result)
--- scrape.py
+++ scrape.py
@@ -1,4 +1,19 @@
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import service
+import time
 
-
+def scrape_website(website):
+    print("Launnching Chrome browser...")
+    
+    chrome_driver_path ="./setup.exe"
+    options = webdriver.ChromeOptions( )
+    driver = webdriver.Chrome(service=service(chrome_driver_path), options=options)
+    
+    try:
+        driver.get(website)
+        print("page loaded...")
+        html = driver.page.source
+        time.sleep(10)
+        return html
+    finally:
+        driver.quit()
