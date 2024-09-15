e1ad26b965edecc0a57c8c7798f8881751e342bb
iamedisonu <iamedisonu250@gmail.com>
2024-09-14 20:18:30 -0500

    webdriver

--- scrape.py
+++ scrape.py
@@ -1,18 +1,19 @@
 import selenium.webdriver as webdriver
-from selenium.webdriver.chrome.service import service
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
 import time
 
 def scrape_website(website):
     print("Launnching Chrome browser...")
     
-    chrome_driver_path ="./setup.exe"
-    options = webdriver.ChromeOptions( )
-    driver = webdriver.Chrome(service=service(chrome_driver_path), options=options)
+    chrome_driver_path ="./msedgedriver.exe"
+    options = Options()
+    driver = webdriver.Edge(service=Service(chrome_driver_path), options = options)
     
     try:
         driver.get(website)
         print("page loaded...")
-        html = driver.page.source
+        html = driver.page_source
         time.sleep(10)
         return html
     finally:
